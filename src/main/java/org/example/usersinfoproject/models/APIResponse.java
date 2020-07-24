27b27edb4e193843b513548725de3361854a87ef
Abhijith K S <abhijithks6400@gmail.com>
2020-07-24 11:49:57 +0530

    modified model classes

--- src/main/java/org/example/usersinfoproject/models/APIResponse.java
+++ src/main/java/org/example/usersinfoproject/models/APIResponse.java
@@ -2,9 +2,13 @@ package org.example.usersinfoproject.models;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class APIResponse {
+    @NotNull
     private Integer status;
+    @NotNull
     private String message;
     private Object data;
 }
--- src/main/java/org/example/usersinfoproject/models/User.java
+++ src/main/java/org/example/usersinfoproject/models/User.java
@@ -2,13 +2,18 @@ package org.example.usersinfoproject.models;
 
 import lombok.Data;
 import org.bson.types.ObjectId;
+import org.hibernate.validator.constraints.Email;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
 public class User {
     private ObjectId id;
+    @NotNull
     private String name;
+    @Email
+    private String email;
     private Integer age;
     private Date createdAt;
     private Date lastActiveTime;
